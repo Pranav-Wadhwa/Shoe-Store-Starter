01eed7105a02c8004c5e7e53a4ae04a76bd24086
Pranav-Wadhwa <pranavwadhwa@outlook.com>
2018-12-28 16:42:55 -0500

    Initial Commit

---
+++ Shoe Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shoe Store
+//
+//  Created by Pranav Wadhwa on 12/28/18.
+//  Copyright © 2018 Pranav Wadhwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
