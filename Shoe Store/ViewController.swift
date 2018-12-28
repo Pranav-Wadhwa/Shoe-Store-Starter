bdb9dc30e0d6b315a85cf443e73dcff3c492da72
Pranav-Wadhwa <pranavwadhwa@outlook.com>
2018-12-28 17:12:20 -0500

    Initial Commit

--- Shoe Store/ViewController.swift
+++ Shoe Store/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Data Setup
+    
+    struct Shoe {
+        var name: String
+        var price: Double
+    }
+    
+    let shoeData = [
+        Shoe(name: "Nike Air Force 1 High LV8", price: 110.00),
+        Shoe(name: "adidas Ultra Boost Clima", price: 139.99),
+        Shoe(name: "Jordan Retro 10", price: 190.00),
+        Shoe(name: "adidas Originals Prophere", price: 49.99),
+        Shoe(name: "New Balance 574 Classic", price: 90.00)
+    ]
+    
+    // Storyboard outlets
+    
+    @IBOutlet weak var shoePickerView: UIPickerView!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
+    @IBAction func buyShoeTapped(_ sender: UIButton) {
+        
+        // Open Apple Pay purchase
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        shoePickerView.delegate = self
+        shoePickerView.dataSource = self
+        
     }
-
-
+    
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    // MARK: - Pickerview update
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return shoeData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return shoeData[row].name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let priceString = String(format: "%.02f", shoeData[row].price)
+        priceLabel.text = "Price = $\(priceString)"
+    }
+}
